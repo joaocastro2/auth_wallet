5392e66b2dcd4b46f543f177b87790560d798f7b
Joao Castro <devjoaocastro2@gmail.com>
2026-09-24 17:14:55 -0300

    first commit

---
+++ src/test/java/com/joaocastro/auth_wallet/AuthWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocastro.auth_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
